8b48dcbd9b958f0b28dfc284f34a37f975aa35dd
ToledoVitor <usedbr@gmail.com>
2023-02-09 21:30:50 -0300

    setting up database

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
+
 # Makes server reload in real time
 app.debug = True
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -35,6 +45,29 @@ def url_parameters(name: str, age: int):
     if age < 18:
         return jsonify(message=f'sorry, {name}, you are not old enough'), 403
     return jsonify(message=f'welcome {name}, you are old enough'), 200
+
+
+# database models
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
 
 
 if __name__ == '__main__':
